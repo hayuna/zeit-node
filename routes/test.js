d977652dd747a0d9656b35438a8473fec9489b3f
mras2303 <marek.kowalonek@gmail.com>
2019-07-16 19:33:11 +0200

    ad m123 method

--- routes/test.js
+++ routes/test.js
@@ -8,9 +8,13 @@ router.route('/')
     .get(Controller.index)
     .post(validateBody(schema.test.post), Controller.add)
 
+router.route('/m123')
+    .get(Controller.m123)
+
 router.route('/:id')
     .get(validateParam(schema.id, 'id'), Controller.get)
     .put([validateParam(schema.id, 'id'), validateBody(schema.test.put)], Controller.replace)
     .patch([validateParam(schema.id, 'id'), validateBody(schema.test.patch)], Controller.update)
+
 
 export default router
